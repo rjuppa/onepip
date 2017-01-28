80cbf3f416534fd257d1ddc8800d7817e7327db1
Radek Juppa <rjuppa@gmail.com>
2017-01-28 01:43:27 +0100

    fixed proj.path

--- src/onepip_proj/wsgi.py
+++ src/onepip_proj/wsgi.py
@@ -6,7 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
-import os
+import os, sys
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.append(os.path.dirname(PROJECT_ROOT) )
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "onepip_proj.settings.production")
 
 from django.core.wsgi import get_wsgi_application
